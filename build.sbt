76953df4d1e083b89bbce780aff461e43e9e2c16
Eragon35 <antipin35@yahoo.com>
2021-08-11 16:29:41 +0300

    Initial commit + greeting page

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "MISPI-Lab3.1"
+
+version := "0.1"
+
+scalaVersion := "2.12.0"
+
+libraryDependencies += "au.com.dius.pact.provider" % "junit" % "4.2.5"
---
+++ src/main/scala/prog/util/Hello.scala
@@ -0,0 +1,15 @@
+package prog.util
+
+import scala.io.Source
+
+object Hello {
+  private val asciiURL = "../../resources/ascii.txt"
+  private val name = "Antipin Arsentii"
+  def greet(): String = {
+    var greeting: String = s"$name's Computer engineer basics Lab № 3\n"
+    val source = Source.fromURL(getClass.getResource(asciiURL))
+    greeting += source.getLines().mkString("\n")
+    source.close()
+    greeting
+  }
+}
